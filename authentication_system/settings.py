0bfb5a256b1767c543f4180a4d0261cda405a4bf
Rahul Saini <rahulsaini5123@outlook.com>
2024-08-04 17:49:40 +0530

    setting update

--- authentication_system/settings.py
+++ authentication_system/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1','43.204.221.167']
+if DEBUG:
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
